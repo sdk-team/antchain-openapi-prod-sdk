aac1b5dde4767663e8cf8d38cf6b52947352f088
sdk-team <sdk-team@alibabacloud.com>
2020-08-04 09:11:54 +0800

    Generated SDK for REALPERSON.

--- REALPERSON/java/ChangeLog.txt
+++ REALPERSON/java/ChangeLog.txt
@@ -1,3 +1,6 @@
+2020-08-04 Version: 1.1.1
+- Generated SDK for REALPERSON.
+
 2020-08-03 Version: 1.1.1
 - Generated SDK for REALPERSON.
 
--- REALPERSON/java/src/main/java/com/alipay/realperson/Client.java
+++ REALPERSON/java/src/main/java/com/alipay/realperson/Client.java
@@ -94,7 +94,7 @@ public class Client {
                     new TeaPair("access_key", _accessKeyId),
                     new TeaPair("charset", "UTF-8"),
                     new TeaPair("baseSdkVersion", "Tea-SDK"),
-                    new TeaPair("sdkVersion", "Tea-SDK-20200803")
+                    new TeaPair("sdkVersion", "Tea-SDK-20200804")
                 );
                 if (!com.aliyun.teautil.Common.empty(_securityToken)) {
                     request_.query.put("security_token", _securityToken);
