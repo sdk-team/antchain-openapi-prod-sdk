187281275bfbe09f2c6b3b7f87c51ae26b620a45
sdk-team <sdk-team@alibabacloud.com>
2020-08-12 16:45:11 +0800

    Generated SDK for REALPERSON.

--- REALPERSON/csharp/core/Properties/AssemblyInfo.cs
+++ REALPERSON/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bb271d40-dc3d-11ea-a63a-d7e675f653cd")]
+[assembly: Guid("0401a610-dc76-11ea-a481-39141d6d490a")]
 
 // Version information for an assembly consists of the following four values:
 //
