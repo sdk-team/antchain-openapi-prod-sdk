2e81c2ad19b31ff0b1c3f5ee92b17d290bb68506
sdk-team <sdk-team@alibabacloud.com>
2021-01-19 01:05:25 +0800

    Generated SDK for DEMO.

--- DEMO/csharp/ChangeLog.md
+++ DEMO/csharp/ChangeLog.md
@@ -1,3 +1,6 @@
+2021-01-19 Version: 2.4.0
+- Generated SDK for DEMO.
+
 2020-12-30 Version: 2.4.1
 - Generated SDK for DEMO.
 
--- DEMO/csharp/core/Properties/AssemblyInfo.cs
+++ DEMO/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("da34f4e0-4a43-11eb-8f8e-e56fae069350")]
+[assembly: Guid("3e794000-59ad-11eb-b829-1d264d9c530d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("2.4.1.0")]
+[assembly: AssemblyFileVersion("2.4.1.0")]
